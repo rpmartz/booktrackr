d65b5f38f0b73d778627ec74e4d3dc4adb956bbe
Ryan Martz <martzrp@gmail.com>
2016-04-23 14:07:33 -0400

    initial

---
+++ src/main/java/com/ryanpmartz/booktrackr/BooktrackrApplication.java
@@ -0,0 +1,12 @@
+package com.ryanpmartz.booktrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktrackrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ryanpmartz/booktrackr/BooktrackrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ryanpmartz.booktrackr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooktrackrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
