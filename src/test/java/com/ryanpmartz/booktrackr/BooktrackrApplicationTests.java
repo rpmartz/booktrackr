3b5d61bbc8fab7fc04da16621d86ea6677af8dea
Ryan Martz <martzrp@gmail.com>
2016-04-23 18:23:59 -0400

    comimit to trigger build

--- src/test/java/com/ryanpmartz/booktrackr/BooktrackrApplicationTests.java
+++ src/test/java/com/ryanpmartz/booktrackr/BooktrackrApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class BooktrackrApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
+	public void contextLoads() {}
 
 }
