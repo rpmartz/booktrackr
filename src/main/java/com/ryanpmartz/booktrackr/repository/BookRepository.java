b8306c4527d79d14569f279668f34e0c00718650
Ryan Martz <martzrp@gmail.com>
2016-05-01 15:15:55 -0400

    extend jpa repository instead of crud repository

--- src/main/java/com/ryanpmartz/booktrackr/repository/BookRepository.java
+++ src/main/java/com/ryanpmartz/booktrackr/repository/BookRepository.java
@@ -1,9 +1,9 @@
 package com.ryanpmartz.booktrackr.repository;
 
 import com.ryanpmartz.booktrackr.domain.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
