51a474f954c51543f79fee115e0c7d92004c3c16
Ryan Martz <martzrp@gmail.com>
2016-05-04 07:42:51 -0400

    remove context-dependent test and add integration test

---
+++ src/integration-test/java/com/ryanpmartz/booktrackr/controller/BookControllerIntTest.java
@@ -0,0 +1,35 @@
+package com.ryanpmartz.booktrackr.controller;
+
+import com.ryanpmartz.booktrackr.BooktrackrApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BooktrackrApplication.class)
+@WebAppConfiguration
+public class BookControllerIntTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
