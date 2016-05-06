5081b954f136b0d32247bc3a3feb67e7385c2b61
Ryan Martz <martzrp@gmail.com>
2016-05-05 20:33:07 -0400

    back out equals and hash code for now

--- src/main/java/com/ryanpmartz/booktrackr/domain/Book.java
+++ src/main/java/com/ryanpmartz/booktrackr/domain/Book.java
@@ -74,25 +74,4 @@ public class Book implements Serializable {
                 ", author='" + author + '\'' +
                 '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Book book = (Book) o;
-
-        if (!id.equals(book.id)) return false;
-        if (!title.equals(book.title)) return false;
-        return author.equals(book.author);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + author.hashCode();
-        return result;
-    }
 }
