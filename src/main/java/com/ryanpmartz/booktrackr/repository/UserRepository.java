066d2b4d9b9e7113d8e8825ef9bc08786c26928b
Ryan Martz <martzrp@gmail.com>
2016-05-11 06:33:24 -0400

    add user service

--- src/main/java/com/ryanpmartz/booktrackr/repository/UserRepository.java
+++ src/main/java/com/ryanpmartz/booktrackr/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByEmail(String email);
 }
---
+++ src/main/java/com/ryanpmartz/booktrackr/service/UserService.java
@@ -0,0 +1,34 @@
+package com.ryanpmartz.booktrackr.service;
+
+import com.codahale.metrics.annotation.Timed;
+import com.ryanpmartz.booktrackr.domain.User;
+import com.ryanpmartz.booktrackr.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Timed
+    @Transactional(readOnly = true)
+    public Optional<User> getUserByEmail(String email) {
+        return Optional.of(userRepository.findByEmail(email));
+    }
+
+    @Timed
+    @Transactional
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
