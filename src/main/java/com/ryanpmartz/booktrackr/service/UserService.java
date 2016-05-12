11e998698936dc28e6d4bc8b8e8506d2895ca3d4
Ryan Martz <martzrp@gmail.com>
2016-05-12 06:51:59 -0400

    fix weird space

--- src/main/java/com/ryanpmartz/booktrackr/service/UserService.java
+++ src/main/java/com/ryanpmartz/booktrackr/service/UserService.java
@@ -15,7 +15,6 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Autowired
-
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
