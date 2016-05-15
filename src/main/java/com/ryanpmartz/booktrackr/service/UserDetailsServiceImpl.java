92cd7f0d313a6a31a868d165c5e71e75c5f48d8d
Ryan Martz <martzrp@gmail.com>
2016-05-15 07:49:01 -0400

    UserDetailsService implementation

---
+++ src/main/java/com/ryanpmartz/booktrackr/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ryanpmartz.booktrackr.service;
+
+import com.ryanpmartz.booktrackr.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> result = userService.getUserByEmail(username);
+
+        return result.orElse(null);
+    }
+}
