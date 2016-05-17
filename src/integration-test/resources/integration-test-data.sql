406a0a0279571215dc1f472d9aa8a0159a59af63
Ryan Martz <martzrp@gmail.com>
2016-05-16 21:09:28 -0400

    update book integration test

--- src/integration-test/resources/integration-test-data.sql
+++ src/integration-test/resources/integration-test-data.sql
@@ -7,7 +7,7 @@ WHERE id IS NOT NULL;
 
 INSERT INTO users (id, created, modified, version, email, enabled, first_name, last_name, password)
 VALUES ('abee7ba3-8bf7-496c-a19a-de0471fc06c1', '2016-03-26 21:08:58.551000', '2016-03-26 21:08:58.551000', 0,
-        'martzrp@gmail.com',
+        'booktrackr@ryanpmartz.com',
         TRUE, 'Ryan', 'Martz',
         '$2a$10$Fo4wvoGJUAGV5POUr7hDD.bKNrLuQhQU9.IH7YY93Lv7RX4Zd4ZqK');
 
