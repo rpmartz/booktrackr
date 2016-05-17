f4519502de74e9a4898290f600f9a79936ca4e82
Ryan Martz <martzrp@gmail.com>
2016-05-16 20:33:57 -0400

    add user to new book

--- src/main/java/com/ryanpmartz/booktrackr/service/BookService.java
+++ src/main/java/com/ryanpmartz/booktrackr/service/BookService.java
@@ -1,8 +1,11 @@
 package com.ryanpmartz.booktrackr.service;
 
 import com.codahale.metrics.annotation.Timed;
+import com.ryanpmartz.booktrackr.authentication.JwtUtil;
 import com.ryanpmartz.booktrackr.domain.Book;
+import com.ryanpmartz.booktrackr.domain.User;
 import com.ryanpmartz.booktrackr.repository.BookRepository;
+import com.ryanpmartz.booktrackr.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +18,12 @@ import java.util.UUID;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, UserRepository userRepository) {
         this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
     }
 
     @Timed
@@ -36,6 +41,8 @@ public class BookService {
     @Timed
     @Transactional
     public Book createBook(Book book) {
+        User user = userRepository.findOne(JwtUtil.tokenFromSecurityContext().getUserId());
+        book.setUser(user);
         return bookRepository.save(book);
     }
 
