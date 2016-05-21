1fb51dc1e02509f6070c5d58f51a1b9f12758b67
Ryan Martz <martzrp@gmail.com>
2016-05-21 10:08:45 -0400

    exclude bootstrap js

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,13 @@ gulp.task('default', function () {
     console.log('Gulp is working.');
 });
 
+// todo add excludes for bootstrap
 gulp.task('wiredep', function () {
     return gulp.src('./src/main/resources/static/index.html')
-        .pipe(wiredep())
+        .pipe(wiredep({
+            exclude: [
+                'bower_components/bootstrap/dist/js/' // use ui-bootstrap
+            ]
+        }))
         .pipe(gulp.dest('./build/resources/main/resources/static'));
 });
