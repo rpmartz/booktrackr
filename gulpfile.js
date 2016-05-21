b7245641fabadd45be060170f85572ccfdd93f87
Ryan Martz <martzrp@gmail.com>
2016-05-20 20:04:43 -0400

    add wiredep task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
 
 gulp.task('default', function () {
     console.log('Gulp is working.');
+});
+
+gulp.task('wiredep', function () {
+    return gulp.src('./src/main/resources/static/index.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('./build/resources/main/resources/static'));
 });
