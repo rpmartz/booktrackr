5d47baeac4ffaac87b02f12b9d5d90d90785fbf9
Ryan Martz <martzrp@gmail.com>
2016-05-22 07:38:13 -0400

    add override so bootstrap css is injected

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,19 @@ gulp.task('default', function () {
     console.log('Gulp is working.');
 });
 
-// todo add excludes for bootstrap
 gulp.task('wiredep', function () {
-    return gulp.src('./src/main/resources/static/index.html')
+    return gulp.src('src/main/resources/static/index.html')
         .pipe(wiredep({
             exclude: [
                 'bower_components/bootstrap/dist/js/' // use ui-bootstrap
-            ]
+            ],
+            overrides: { // see discussion here https://github.com/twbs/bootstrap/issues/16663
+                bootstrap: {
+                    main: [
+                        "dist/css/bootstrap.css"
+                    ]
+                }
+            }
         }))
         .pipe(gulp.dest('./build/resources/main/resources/static'));
 });
