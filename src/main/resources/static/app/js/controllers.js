8915186c63c82d775b3d9b11849976854ffc9a9d
Ryan Martz <martzrp@gmail.com>
2016-07-04 14:57:22 -0400

    add books controller

--- src/main/resources/static/app/js/controllers.js
+++ src/main/resources/static/app/js/controllers.js
@@ -3,7 +3,9 @@
     angular
         .module('booktrackrApp')
         .controller('HomeController', HomeController)
-        .controller('SignupController', SignupController);
+        .controller('SignupController', SignupController)
+        .controller('LoginController', LoginController)
+        .controller('BooksController', BooksController);
 
     HomeController.$inject = ['Book', '$log'];
     function HomeController(Book, $log) {
@@ -17,9 +19,57 @@
         })
     }
 
-    SignupController.$inject = [];
-    function SignupController() {
+    SignupController.$inject = ['AuthService', '$log'];
+    function SignupController(AuthService, $log) {
         var vm = this;
+
+        vm.newUser = {};
+
+        vm.signup = function () {
+            AuthService.signup(vm.newUser)
+                .then(function (res) {
+                        $log.info('Signup succeeded', res);
+                    },
+                    function (err) {
+                        $log.error('Signup failed ', err);
+                    })
+
+        }
+
+    }
+
+    LoginController.$inject = ['AuthService', '$log', '$location', 'TokenService'];
+    function LoginController(AuthService, $log, $location, TokenService) {
+        var vm = this;
+        vm.credentials = {};
+
+        vm.authenticate = function () {
+            AuthService.login(vm.credentials)
+                .then(
+                    function (res) {
+                        var jwt = res.headers('Authorization');
+                        TokenService.setCurrentUserToken(jwt);
+                        $location.path('/books');
+                    },
+                    function (err) {
+                        $log.debug('Auth Failed');
+                        // todo show failed auth message
+                    })
+        }
+
+
+    }
+
+    BooksController.$inject = ['Book', '$log'];
+    function BooksController(Book, $log) {
+        var vm = this;
+
+        Book.all().then(function (res) {
+            $log.debug('Books response: ', res);
+
+        }, function (err) {
+            $log.error('all books call failed', err);
+        });
     }
 
 })();
