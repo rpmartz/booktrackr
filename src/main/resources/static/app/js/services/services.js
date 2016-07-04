1a5ba276ba3b4d8c3acb17738de64b771d3bfe57
Ryan Martz <martzrp@gmail.com>
2016-07-04 07:33:20 -0400

    add login and authenticate methods to AuthService

--- src/main/resources/static/app/js/services/services.js
+++ src/main/resources/static/app/js/services/services.js
@@ -2,7 +2,6 @@
     'use strict';
 
     angular.module('booktrackrApp').factory('Book', Book);
-
     Book.$inject = ['$http'];
 
     function Book($http) {
@@ -12,6 +11,26 @@
 
         function allBooks() {
             return $http.get('/books');
+        }
+
+        return service;
+    }
+
+    angular.module('booktrackrApp').factory('AuthService', AuthService);
+    AuthService.$inject = ['$http'];
+
+    function AuthService($http) {
+        var service = {
+            login: login,
+            signup: signup
+        };
+
+        function login(credentials) {
+            return $http.post('/authenticate', credentials);
+        }
+
+        function signup(signupForm) {
+            return $http.post('/users', signupForm);
         }
 
         return service;
