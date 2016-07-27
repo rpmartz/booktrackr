3ce5289ec8200f391a13c14d673ee02c0d1b4d23
Ryan Martz <martzrp@gmail.com>
2016-07-27 05:37:42 -0400

    add other dependencies to karma config

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -8,7 +8,14 @@ module.exports = function (config) {
 
         files: [
             'src/main/resources/static/bower_components/angular/angular.js',
+            'src/main/resources/static/bower_components/angular-messages/angular-messages.js',
+            'src/main/resources/static/bower_components/angular-mocks/angular-mocks.js',
             'src/main/resources/static/bower_components/angular-route/angular-route.js',
+            'src/main/resources/static/bower_components/a0-angular-storage/dist/angular-storage.js',
+            'src/main/resources/static/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'src/main/resources/static/bower_components/angular-sanitize/angular-sanitize.js',
+            'src/main/resources/static/bower_components/showdown/src/showdown.js',
+            'src/main/resources/static/bower_components/angular-markdown-directive/markdown.js',
             'src/main/resources/static/app/js/**/*.js',
             'src/test/javascript/spec/**/*.js'
         ]
