7645bf07f8fc4f84329da68cb9c460ada6b98639
Ryan Martz <martzrp@gmail.com>
2016-07-28 06:29:49 -0400

    failing test but seems to be sset up correctly

--- src/test/javascript/spec/controllerSpec.js
+++ src/test/javascript/spec/controllerSpec.js
@@ -1,4 +1,35 @@
-describe('First Unit Test', function () {
+describe('Controller Tests', function () {
 
     beforeEach(angular.mock.module('booktrackrApp'));
+
+
+    // beforeEach(angular.mock.inject(function(_$controller_){
+    //     $controller = _$controller_;
+    // }));
+
+    describe('HomeControllerTests', function () {
+        var HomeController, MockBookService, $scope;
+
+
+        beforeEach(inject(function ($q, $controller, $rootScope) {
+            $scope = $rootScope.$new();
+
+
+            MockBookService = {
+                all: function () {
+                    var deferred = $q.defer();
+                    deferred.resolve([]);
+                    return deferred.promise;
+                }
+            };
+
+            HomeController = $controller('HomeController', {Book: MockBookService, $scope: $scope});
+
+        }));
+
+        it('should have "books" defined', function () {
+            $scope.$apply();
+            expect(HomeController.books).toBeDefined();
+        })
+    });
 });
