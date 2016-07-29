5dea14698b0bf9d248ca69410da6ceb313214cbd
Ryan Martz <martzrp@gmail.com>
2016-07-28 21:09:25 -0400

    add test for ViewBookController

--- src/test/javascript/spec/controllerSpec.js
+++ src/test/javascript/spec/controllerSpec.js
@@ -27,4 +27,29 @@ describe('Controller Tests', function () {
             expect(HomeController.books).toBeDefined();
         })
     });
+
+    describe('ViewBookController Test', function () {
+        var ViewBookController, MockBookService, $scope;
+
+        beforeEach(inject(function ($q, $controller, $rootScope) {
+            $scope = $rootScope.$new();
+
+            MockBookService = {
+                getBook: function () {
+                    var deferred = $q.defer();
+                    deferred.resolve({data: {}});
+                    return deferred.promise;
+                }
+            };
+
+            ViewBookController = $controller('ViewBookController', {Book: MockBookService, $scope: $scope});
+
+        }));
+
+        it('should have "book" defined', function () {
+            $scope.$apply();
+            expect(ViewBookController.book).toBeDefined();
+        })
+    });
+
 });
