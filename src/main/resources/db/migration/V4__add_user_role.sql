e8e7bd227cbebbf2352d5d0aa2875c887c5b40ee
Ryan Martz <martzrp@gmail.com>
2018-01-20 11:32:12 -0500

    fix user_id type in flyway sql

--- src/main/resources/db/migration/V4__add_user_role.sql
+++ src/main/resources/db/migration/V4__add_user_role.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS authorities
   modified  TIMESTAMP          NOT NULL,
   version   INTEGER,
   authority VARCHAR(255),
-  user_id   UUID               NOT NULL,
+  user_id   BIGINT             NOT NULL,
   CONSTRAINT fk_s21btj9mlob1djhm3amivbe5e FOREIGN KEY (user_id) REFERENCES users (id)
 );
